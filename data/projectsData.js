05885f291f4f8accf70ccd4ae6087ecfbcca9684
useonglee <useong0830@naver.com>
2022-02-25 16:08:04 +0900

    post: Add react-season-component project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,11 +1,19 @@
 const projectsData = [
   {
+    title: 'react-season-component',
+    description: `날씨, 계절, 시간에 따른 아름다운 효과를 주고싶을 때 사용할 수 있는 오픈 소스 라이브러리 입니다.`,
+    imgSrc: '/static/images/projects/react-season-component-logo.png',
+    href: 'https://ppo-f-man.github.io/react-season-component-web',
+    github: 'https://github.com/PPO-F-MAN/react-season-component',
+  },
+  {
     title: 'Reciper',
     description: `팀원모집 서비스와 실시간 협업툴을 함께 제공하는 플랫폼입니다. 
     팀원 모집 게시글을 통해 같이 사이드 프로젝트를 할 팀원을 찾아 보세요 :)     
     채팅, 칸반보드, 캘린더 등을 통해 협업을 진행할 수 있습니다!`,
-    imgSrc: '/static/images/reciper.png',
+    imgSrc: '/static/images/projects/reciper.png',
     href: 'https://reciper.me',
+    github: 'https://github.com/codestates/Reciper-client',
   },
 ]
 
