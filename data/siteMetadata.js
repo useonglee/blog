e924f5d805bb20ebfb1586de7e39f2533bc2b301
useonglee <useong0830@naver.com>
2022-02-27 23:42:13 +0900

    feat: 웹페이지 Google Analytics 연결

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,9 +18,9 @@ const siteMetadata = {
   locale: 'ko-KR',
   analytics: {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'useonglee.dev', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-2N6E76M3X2', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo
